6ad98f3d1702720a470deee605e36c6739d49a42
Marius <marius.kriegerowski@zmaw.de>
2015-01-26 17:18:14 +0100

    run_all()

--- run.py
+++ run.py
@@ -19,17 +19,19 @@ _logger.info('Start logging')
 if __name__ == '__main__':
     webnet = os.environ['WEBNET']
     r = Reader(webnet,
-               data='mseed/2008/KAC/*',
+               data='mseed/2008/*/*',
                events='catalog/intern/Oct2008_events.pf', 
                phases='catalog/intern/Oct2008_phases.pf')
     r.start()
 
     config = RapidinvConfig(base_path=pjoin(webnet, 'inversions'), 
-                            fn_stations=pjoin(webnet, 'meta/stations.pf'))
+                            fn_stations=pjoin(webnet, 'meta/stations.pf'),
+                            reset_time=True)
     
     inversion = MultiEventInversion(config=config, 
                                     reader=r)
     inversion.prepare(force=True,
-                      num_inversions=10)
+                      num_inversions=3)
 
+    inversion.run_all()
 _logger.info('finished')
